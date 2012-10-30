4d9469b4c84c830a9c15befdb90145d82dd2f9c0
Doug Binks <doug.binks@gmail.com>
2012-10-30 15:08:07 +0100

    First commit of combined GLEW and GLFW

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+This is a simple concatanation of GLEW and GLFW, for which the respective copyrights/licenses are included.
+
+Versions:
+
+glfw-2.7.6
+glew-1.9.0
+
---
+++ include/GL/glewfw.h
@@ -0,0 +1,17 @@
+//
+//  glewfw.h
+//  glewfw
+//
+//  Created by Doug Binks on 10/29/12.
+//  Copyright (c) 2012 Doug Binks. All rights reserved.
+//
+
+#pragma once
+#ifndef glewfw_glewfw_h
+#define glewfw_glewfw_h
+
+#include <GL/glew.h>
+#include <GL/glfw.h>
+
+
+#endif
